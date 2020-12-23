bdea1617aefe477fa574733e793cf29be4fbd013
ahn_mac <licors214@gmail.com>
2020-12-23 18:40:45 +0900

    Optional 이용한 널 처리

--- src/main/java/com/example/jpa/repository/MyRepository.java
+++ src/main/java/com/example/jpa/repository/MyRepository.java
@@ -3,9 +3,11 @@ package com.example.jpa.repository;
 import com.example.jpa.Comment;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 // 임의 구현시 공통부분 인터페이스로 따로 빼고 싶을 때 아래와 같이 구현
 @NoRepositoryBean
@@ -13,4 +15,9 @@ public interface MyRepository<T, Id extends Serializable> extends Repository<T, 
     <E extends T> E save(E comment);
 
     List<T> findAll();
+
+    long count();
+
+    @Nullable
+    <E extends T> Optional<E> findById(Id id);
 }
--- src/test/java/com/example/jpa/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/jpa/repository/CommentRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +27,10 @@ public class CommentRepositoryTest {
 
         List<Comment> comments = commentRepository.findAll();
         assertThat(comments.size()).isEqualTo(1);
+
+        Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId).isEmpty();
+        // Optional 이용한 예외처리 구현 코드
+//        Comment byIdComment = byId.orElseThrow(IllegalAccessError::new);
     }
 }
