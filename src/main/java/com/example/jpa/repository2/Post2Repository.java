d38266bcf0f9a6aecb7185cd1986c54a93f53aed
DESKTOP-UGTRB8L\Ahn <licors214@gmail.com>
2020-12-27 15:35:03 +0900

    QueryDSL 세팅 및 사용법

--- src/main/java/com/example/jpa/repository2/Post2Repository.java
+++ src/main/java/com/example/jpa/repository2/Post2Repository.java
@@ -1,6 +1,7 @@
 package com.example.jpa.repository2;
 
 import com.example.jpa.AllEntityRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface Post2Repository extends AllEntityRepository<Post2, Long> {
+public interface Post2Repository extends AllEntityRepository<Post2, Long>, QuerydslPredicateExecutor<Post2> {
 }
--- src/test/java/com/example/jpa/repository2/Post2RepositoryTest.java
+++ src/test/java/com/example/jpa/repository2/Post2RepositoryTest.java
@@ -1,12 +1,16 @@
 package com.example.jpa.repository2;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -27,10 +31,21 @@ public class Post2RepositoryTest {
 
         post2Repository.save(post2);
 
+        Predicate predicate = QPost2.post2.title.containsIgnoreCase("hib");
+        Optional<Post2> one = post2Repository.findOne(predicate);
+        assertThat(one).isNotEmpty();
+
+        // 위 코드 축약
+        Optional<Post2> hib = post2Repository.findOne(QPost2.post2.title.containsIgnoreCase("hib"));
+        assertTrue(hib.isPresent());
+
+
         // Persistent 상태
         assertThat(post2Repository.contains(post2)).isTrue();
         post2Repository.delete(post2);
         // 롤백 되기 때문에 삭제 강제로 실행시키기 위해 flush()
         post2Repository.flush();
+
+
     }
 }
