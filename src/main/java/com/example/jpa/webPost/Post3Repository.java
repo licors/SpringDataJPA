8307f31b3da6edae84dccf3de8dbaa9b8db377f3
DESKTOP-UGTRB8L\Ahn <licors214@gmail.com>
2020-12-29 18:01:57 +0900

    @Query 에서 Named Parameter, SpEL 기능

--- src/main/java/com/example/jpa/webPost/Post3Repository.java
+++ src/main/java/com/example/jpa/webPost/Post3Repository.java
@@ -3,6 +3,7 @@ package com.example.jpa.webPost;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +14,8 @@ public interface Post3Repository extends JpaRepository<Post3, Long> {
 
     List<Post3> findByTitle(String title, Sort sort);
 
-    @Query(value = "select p FROM Post3 as p where p.created > ?1")
-    List<Post3> findByCreatedAfter(Date date, Sort sort);
+    // :created 는 Named Parameter 기능
+    // #{#entityName} 는 SpEL, 위에 엔티티 이름 가져와서 사용
+    @Query(value = "select p FROM #{#entityName} as p where p.created > :created")
+    List<Post3> findByCreatedAfter(@Param("created") Date date, Sort sort);
 }
